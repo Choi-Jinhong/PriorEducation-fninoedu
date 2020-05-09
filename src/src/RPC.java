6a4062748e4571aa2c9bfb62fe43918969fb9dee
Choi_Jinhong <chlwlsghd58@gmail.com>
2020-05-09 09:08:30 +0900

    가위 바위 보 게임

---
+++ src/src/RPC.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.Scanner;
+
+public class RPC {
+    public static void main(String[] args) {
+        int user, com;
+        System.out.println("가위(0), 바위(1), 보(2) 중 하나를 입력해 주세요.:");
+        user = new Scanner(System.in).nextInt();
+        com = (int)(Math.random()*3);
+        System.out.printf("컴퓨터가 선택한 것은 %s 입니다.", com);
+        int result = user - com;
+        if(result == 1 || result == -2)
+            System.out.println("유저 승리");
+        else if(result == 0)
+            System.out.println("무승부");
+        else
+            System.out.println("유저 패배");
+    }
+}
